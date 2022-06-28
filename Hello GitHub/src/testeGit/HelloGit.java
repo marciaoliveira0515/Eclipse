50540348b0152477814115346479f022fa58fa97
Márcia Beserra <marciathata@gmail.com>
2022-06-28 17:47:06 -0300

    Add arquivos via upload

---
+++ Hello GitHub/src/testeGit/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package testeGit;
+
+/**
+ * @author Márcia
+ *Exemplo de Integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Git");
+
+	}
+
+}
